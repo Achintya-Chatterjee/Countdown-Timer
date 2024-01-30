ed1754b7a418ea0e873a262a166033c37c4fb4f4
Achintya-Chatterjee <achintyachatterjee.jara@gmail.com>
2024-01-30 07:33:23 +0530

    feat:date implemented

--- js/app.js
+++ js/app.js
@@ -1,3 +1,17 @@
-const endDate="30 April 2024 2:05 AM";
+const endDate = "30 April 2024 2:05 PM";
 
-document.getElementById("end-date").innerText=endDate
+document.getElementById("end-date").innerText = endDate;
+const inputs = document.querySelectorAll("input");
+//const clock = ()=>{
+
+//}
+
+function clock() {
+  const end = new Date(endDate);
+  const now = new Date();
+  const diff = (end - now) / 1000;
+  console.log(diff);
+  inputs[0].value = Math.floor(diff / 3600 / 24);
+}
+
+clock();
