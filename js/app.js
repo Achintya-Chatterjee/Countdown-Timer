a8560374af026ea877bc54660dd072f3d598d634
Achintya-Chatterjee <achintyachatterjee.jara@gmail.com>
2024-01-30 07:38:24 +0530

    feat:hours implementation done

--- js/app.js
+++ js/app.js
@@ -12,6 +12,7 @@ function clock() {
   const diff = (end - now) / 1000;
   console.log(diff);
   inputs[0].value = Math.floor(diff / 3600 / 24);
+  inputs[1].value = Math.floor(diff / 3600 % 24)
 }
 
 clock();
